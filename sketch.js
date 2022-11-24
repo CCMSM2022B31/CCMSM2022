6d89acf43d9251d688dda6ae1f715885ebaaa5f1
CCMSM2022B31 <118990095+CCMSM2022B31@users.noreply.github.com>
2022-11-24 12:11:52 -0300

    ver 2?????

--- sketch.js
+++ sketch.js
@@ -5,8 +5,21 @@ let xBol=200;
 let yBol=200;
 let diametro=50
 let velocidadex=1;
+    
+let euX=0
+let yRakE=160
+let LRakE=15
+let ARakE=90
+
+let xRakD=385
+let yRakD=160
+let lRakD=15
+let ARakD=90
+
 function draw(){
   background(0);
+  rect(xRakE,yRakE,LRakE,ARakE);
+  rect(xRakD,yRakD,lRakD,ARakD);
   circle(xBol,yBol,diametro);
   xBol +=velocidadex;
   if(xBol+diametro/2>=400){
